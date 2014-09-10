6586b003e1fa52bc5ab2d4894af9af99e9e37fa7
Snoop60 <Triska.t.60@email.cz>
2014-09-10 19:49:05 +0200

    test

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -1,24 +1,15 @@
 <?php
 
-//namespace App\Presenters;
+namespace App\Presenters;
+
+use Nette,
+	App\Model;
+
+
 /**
  * Base presenter for all application presenters.
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
 
-	protected $fileModel;
-	protected $userModel;
-	protected $versionModel;
-	protected $articleModel;
-	protected $commentsModel;
-
-	public function injectBase(Models\File $file, Models\Article $article, Models\Version $version, Models\User $user, Models\Comments $comments)
-	{
-		$this->fileModel = $file;
-		$this->userModel = $user;
-		$this->articleModel = $article;
-		$this->versionModel = $version;
-		$this->commentsModel = $comments;
-	}
 }
--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Mov;
+namespace App;
 
 use Nette,
 	Nette\Application\Routers\RouteList,
 	Nette\Application\Routers\Route,
 	Nette\Application\Routers\SimpleRouter;
+
 
 /**
  * Router factory.
@@ -19,29 +20,8 @@ class RouterFactory
 	public function createRouter()
 	{
 		$router = new RouteList();
-                		$router[] = $adminRouter = new RouteList('Admin');
-        // Admin
-        $router[] = new Route('admin/<presenter>/<action>/<id>', array(
-            'module' => 'Admin',
-            'presenter' => 'Homepage',
-            'action' => 'default',
-            'id' => NULL,
-        ));
-		if (function_exists('apache_get_modules') && in_array('mod_rewrite', apache_get_modules())) {
-			$router[] = new Route('index.php', 'Front:Homepage:default', Route::ONE_WAY);
-
-			$router[] = $baseRouter = new RouteList('Front');
-			$baseRouter[] = new Route('<presenter>/<action>[/<id>]', array(
-				'presenter' => 'Homepage',
-				'action'        => 'default'
-			));
-
-		}else {
-
-			$router = new SimpleRouter('Front:Sign:in');
-		}
+		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
 		return $router;
 	}
 
 }
-
